25299b396bec09baa32eb1d80801ad79ca4c358a
AQ <rqliangf@163.com>
2013-07-01 23:39:43 +0800

    first up

---
+++ fight/roleConfTbl.lua
@@ -0,0 +1,46 @@
+roleConfTbl = {
+	{
+		name = "role1",
+		RoleImgSrc_walk = "image/xuchu/xc_walk.png",
+		RoleImgSrc_att = "image/xuchu/xc_att.png",
+		RoleImgSrc_dead = "image/xuchu/xc_dead.png",
+		HpMax = 200,
+		Hp = 200,
+		Att = 90,
+		Horde = "A",
+	},
+	
+	{
+		name = "role2",
+		RoleImgSrc_walk = "image/xuchu/xc_walk.png",
+		RoleImgSrc_att = "image/xuchu/xc_att.png",
+		RoleImgSrc_dead = "image/xuchu/xc_dead.png",
+		HpMax = 200,
+		Hp = 200,
+		Att = 90,
+		Horde = "A",
+	},
+	
+	{
+		name = "role3",
+		RoleImgSrc_walk = "image/xuchu/xc_walk.png",
+		RoleImgSrc_att = "image/xuchu/xc_att.png",
+		RoleImgSrc_dead = "image/xuchu/xc_dead.png",
+		HpMax = 200,
+		Hp = 200,
+		Att = 90,
+		Horde = "B",
+	},
+	
+	{
+		name = "role4",
+		RoleImgSrc_walk = "image/xuchu/xc_walk.png",
+		RoleImgSrc_att = "image/xuchu/xc_att.png",
+		RoleImgSrc_dead = "image/xuchu/xc_dead.png",
+		HpMax = 200,
+		Hp = 200,
+		Att = 90,
+		Horde = "B",
+	},
+
+}
